a7839922558d27ce5bd8ccf7a228c1a1002d1f4e
ChebanaSofiene <sofiene.chebana@gmail.com>
2025-09-25 23:42:15 +0200

    Added login page + route

---
+++ resources/views/login.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+    <title>Connexion </title>
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+</head>
+
+<body style="background-color:#1D1E1B;">
+    <img 
+    src="/images/logo.webp"
+    style="display: block; margin-left: auto; margin-right: auto; width: 20%; padding-top: 50px; padding-bottom: 50px;"
+    />
+
+    <div style="background-color: #FFFAF1; width: 400px; margin: auto; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+        <form style="text-align: center;">
+            Email
+            <br>
+            <input type="text" style="padding: 10px; width: 300px; margin-bottom: 20px; border-radius: 5px; border-color: black;">
+            <br>
+            Mot de passe
+            <br>
+            <input type="password" style="padding: 10px; width: 300px; margin-bottom: 20px; border-radius: 5px; border-color: black;">
+            <br>
+            <button type="submit" style="padding: 10px 20px; background-color: #DF201D; border-radius: 30px;">Se connecter</button>
+
+        </form>
+    </div>
+
+    <p style ="font-family: 'Archivo Black'; color:white;"> test </p>
+</body>
+</html>
