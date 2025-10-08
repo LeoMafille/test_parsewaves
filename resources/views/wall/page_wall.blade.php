1b99055b434660c8e692e9c05ef708251e58ea59
Grizy12 <glericheux@gmail.com>
2025-10-08 14:48:27 +0200

    salut a tous j'ai add le hachage
    de rien

--- resources/views/wall/page_wall.blade.php
+++ resources/views/wall/page_wall.blade.php
@@ -3,45 +3,56 @@
 
 @section('mur')
 @php 
+   
     $wallFragments = $currentMeasure->wall_parts;
-    $nbFragments = 0;
+    $nbFragments = sizeof($wallFragments);
     $colors = ['red-300', 'blue-300', 'yellow-300', 'purple-300','emerald-300'];
+
+     $url = 'images/wall_pictures/';
+    $hatch = [
+        asset("". $url ."materials1.png"),
+        asset("". $url ."materials2.png"),
+        asset("". $url ."materials3.png")];
+    
     $sumSize = 0;
-    $legende = array();
+    $legend = array();
 @endphp
 @foreach($wallFragments as $wallFragment)
         @php 
-        $nbFragments+=1;
         $sumSize+= $wallFragment->width;
         @endphp
 @endforeach
 <div>
     <h1 class=" text-center font-bold tracking-tight text-3xl ">Mesure {{$currentMeasure->id}}  du chantier {{$currentChantier->id}} </h1>
 </div>
-<div class='flex gap-4 mt-5 h-1/3 w-1/2 md:full mx-auto border-2 border-black'>
+<div class='flex  mt-5 h-1/3 w-1/2 md:full mx-auto border-2 border-black'>
     
 @foreach($wallFragments as $wallFragment)
     @php
+
+        $chosen_hatch = $hatch[$wallFragment->material->type];
         $chosen_color = $colors[array_rand($colors)];
-        $legende[$wallFragment->id] = $chosen_color;
+        $legend[$wallFragment->id] = $chosen_color;
         $key = array_search($chosen_color,$colors);
         unset($colors[$key]);
         //ici on va modifier la taille des carrée en fonction de leurs taille réels.
         $percent = ($wallFragment->width/$sumSize)*100;
+
+        //{{$wallFragment->material->type}}
     @endphp
-    <div class="identification-wall  h-full flex border-2 border-black bg-{{$chosen_color}}" style="width: {{ $percent }}%;" materialInformation="Materiaux: {{$wallFragment->material->name}} : | Profondeur : {{$wallFragment->width}} cm | Informations : {{$wallFragment->material->comment}}|Inflammable : {{$wallFragment->material->flammable}}">
+    {{--<div class="identification-wall  h-full flex border-2 border-black bg-{{$chosen_color}}" style="width: {{ $percent }}%;" materialInformation="Materiaux: {{$wallFragment->material->name}} : | Profondeur : {{$wallFragment->width}} cm | Informations : {{$wallFragment->material->comment}}|Inflammable : {{$wallFragment->material->flammable}} |Type : {{$wallFragment->material->type}}">--}}
+    <div class="identification-wall  h-full flex border-2 border-black bg-{{$chosen_color}} bg-[url({{$chosen_hatch}})] bg-center bg-repeat" style="width: {{ $percent }}%;" materialInformation="Materiaux: {{$wallFragment->material->name}} : | Profondeur : {{$wallFragment->width}} cm | Informations : {{$wallFragment->material->comment}}|Inflammable : {{$wallFragment->material->flammable}} |Type : {{$wallFragment->material->type}}">
     </div>
 @endforeach
 </div>
-
 <div id= "moreInfo" class='mt-5 items-center h-1/5 w-3/5 mx-auto border-2 border-black '>
 
 </div>
 
-<div class='flex flex-wrap gap-2 p-4 mt-5 h-1/5  mx-auto border-2 border-black '>
+<div class='flex flex-wrap gap-2 p-4 mt-5 min-h-1/5  mx-auto border-2 border-black '>
 @foreach($wallFragments as $wallFragment)
     <div class="flex items-center gap-2">
-        <div class="w-3 h-3 bg-{{$legende[$wallFragment->id]}}"></div>
+        <div class="size-12  bg-[url({{$hatch[$wallFragment->material->type]}})] bg-{{$legend[$wallFragment->id]}} bg-[length:100%_100%] "></div>
         <p>Materiaux: {{$wallFragment->material->name}} : | Profondeur : {{$wallFragment->width}} cm | Informations : {{$wallFragment->material->comment}}</p>
     </div>
 @endforeach
